58b567aba14cc169550e29bd7c3d39c6bca00cf6
Matthew Brignola <matthewbrignola@ymail.com>
2022-03-24 15:26:40 -0600

    profile image added

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,13 +1,27 @@
-import { Grid, Container, Typography } from "@mui/material";
+import { Grid, Container, Typography, Box } from "@mui/material";
+import profile from "../img/profile-img.png";
+
+const src = profile;
+const alt = "Image Unavailable";
 
 const About = () => {
   return (
-    <Container sx={{ mt: 10 }}>
+    <Container sx={{ mt: 10, display: "flex", justifyContent: "center" }}>
       <Grid>
         <div className="item">
+          <Box
+            component="img"
+            sx={{
+              display: "block",
+              maxWidth: 300,
+              overflow: "hidden",
+              width: "100%",
+            }}
+            src={src}
+            alt={alt}
+          />
           <Typography variant="h4">Name</Typography>
           <Typography variant="h3">Matthew Brignola</Typography>
-          <img src={"../img/profile-img.png"} alt="Img Unavailable"></img>
         </div>
         <div className="item">
           <Typography variant="h4">Position</Typography>
